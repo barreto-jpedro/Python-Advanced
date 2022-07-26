81692fdb92c00189e0904c6a79c5b8f7b978e804
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-26 10:16:24 -0300

    Moving on with module 3

--- Module 3 - HTTP & APIs/main.py
+++ Module 3 - HTTP & APIs/main.py
@@ -6,9 +6,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+FIRST_LINK = 'https://pt.wikipedia.org/wiki/FSM'
 DRIVER_PATH = '/home/joao/GitHub/Python Advanced/Module 3 - HTTP & APIs/chromedriver'
+
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://en.wikipedia.org/wiki/Linux')
+
+#Initial page
+driver.get(FIRST_LINK)
 print(f"Main page: {driver.title}")
-html_code = driver.page_source
-all_links = driver.find_elements(By.TAG_NAME, "a")
+
+#All the links on first page
+all_elements = driver.find_elements(By.TAG_NAME, "a")
+all_links = [link.get_attribute('href') for link in all_elements ]
+print(f"This page has {len(all_links)} links.")
+
+for link in all_links:
+    try: 
+        if link.find("wikipedia") != -1:
+            driver.get(link)
+            print(100*"_")
+            print(f"URL: {link}")
+            print(f"Title: {driver.title}")
+            print(100*"_")
+    except:
+        print(f"Couldn't get {link}")
+        
